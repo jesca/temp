447b6ceda2d613f8f2fe97b746e5e1223168b5da
jesca <jesca116@gmail.com>
2015-12-21 22:44:46 -0800

    input output (:

---
+++ parse_countries.py
@@ -0,0 +1,16 @@
+import re
+
+with open('a.txt') as f:
+    lines = f.read().splitlines()
+    output = open('output.txt','w+')
+    output.write('{' '\n')
+
+    for line in lines:
+        # get parts of strings in quotes
+        countryAbbr = re.findall(r'"([^"]*)"', line)[0]
+        countryPhoneCode = re.findall(r'\b\d+\b', line)[0]
+        output.write(countryAbbr + ': ' + countryPhoneCode + '\n')
+
+    output.write('}')
+    output.close()
+    f.close()
