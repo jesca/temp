d23d0d6f7bc1ac02c0d2cc482828a2bf704580ce
jesca <jesca116@gmail.com>
2015-12-22 02:16:30 -0800

    added commas

--- parse_countries.py
+++ parse_countries.py
@@ -9,7 +9,7 @@ with open('a.txt') as f:
         # get parts of strings in quotes
         countryAbbr = re.findall(r'"([^"]*)"', line)[0]
         countryPhoneCode = re.findall(r'\b\d+\b', line)[0]
-        output.write(countryAbbr + ': ' + countryPhoneCode + '\n')
+        output.write(countryAbbr + ': ' + countryPhoneCode + ',' + '\n')
 
     output.write('}')
     output.close()
